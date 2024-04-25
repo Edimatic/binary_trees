049affee1c3e45f3743d0f689a1862897e8ef174
Edimatic <ediomiudofia@gmail.com>
2024-04-25 16:44:08 -0700

    Delete

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -28,5 +28,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	newNode->right = NULL;
 
 	/* return a pointer to the node */
-	return newNode;
+	return (newNode);
 }
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -32,5 +32,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	parent->left = new;
 	if (new->left)
 		new->left->parent = new;
-	return new;
+	return (new);
 }
