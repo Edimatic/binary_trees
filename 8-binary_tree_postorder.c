83628d9a34f8ced89cc298e2d7cb65bcf90f6752
Edimatic <ediomiudofia@gmail.com>
2024-04-25 17:13:51 -0700

    Post-order traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,6 +5,7 @@
  * @tree: tree to traverse
  * @func: pointer to a function to call for each node
  */
+
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
